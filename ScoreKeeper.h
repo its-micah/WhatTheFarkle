6f831541f2691a7a4553d28f86993b7430f23ccd
its-micah <mlanier86@gmail.com>
2015-03-21 11:08:33 -0500

    add scorekeeper class

---
+++ ScoreKeeper.h
@@ -0,0 +1,13 @@
+//
+//  ScoreKeeper.h
+//  WhatTheFarkle
+//
+//  Created by Micah Lanier on 3/21/15.
+//  Copyright (c) 2015 Micah Lanier Design and Illustration. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ScoreKeeper : NSObject
+
+@end
---
+++ ScoreKeeper.m
@@ -0,0 +1,13 @@
+//
+//  ScoreKeeper.m
+//  WhatTheFarkle
+//
+//  Created by Micah Lanier on 3/21/15.
+//  Copyright (c) 2015 Micah Lanier Design and Illustration. All rights reserved.
+//
+
+#import "ScoreKeeper.h"
+
+@implementation ScoreKeeper
+
+@end
