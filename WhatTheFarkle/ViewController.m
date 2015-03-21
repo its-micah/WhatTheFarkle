cb9fd076e9edfbe0d6faf4aad8a5bc105356a759
its-micah <mlanier86@gmail.com>
2015-03-21 17:35:38 -0500

    add logans code

--- WhatTheFarkle/ViewController.m
+++ WhatTheFarkle/ViewController.m
@@ -16,8 +16,18 @@
 @property (weak, nonatomic) IBOutlet UILabel *userScore;
 @property (nonatomic, strong) UIDynamicAnimator *animator;
 @property UIView *dieContainer;
+@property NSMutableArray *selectedDiceInTurn;
+
 
 @end
+
+int scoreKeeperInt = 0;
+int one = 0;
+int two = 0;
+int three = 0;
+int four = 0;
+int five = 0;
+int six = 0;
 
 @implementation ViewController
 
@@ -29,22 +39,122 @@
     [self createView];
     [self callDynamics];
     self.dice = [NSMutableArray new];
+    self.selectedDiceInTurn = [NSMutableArray new];
+
 }
 
 - (IBAction)onRollButtonPressed:(id)sender {
     for (DieLabel *label in self.labels) {
         [label roll];
+        [self.selectedDiceInTurn removeAllObjects];
+        one = 0;
+        two = 0;
+        three = 0;
+        four = 0;
+        five = 0;
+        six = 0;
+        [self.selectedDiceInTurn removeAllObjects];
     }
-    UIEvent *shake;
 
-    [self motionBegan:UIEventSubtypeMotionShake withEvent:shake];
     [self callDynamics];
 }
 
 - (void)selectDie:(id)die {
     [self.labels removeObject:die];
     [self.dice addObject:die];
+    [self.selectedDiceInTurn addObject:die];
+    [self scoreMethod];
 }
+
+-(void)scoreMethod
+{
+    for (DieLabel *die in self.selectedDiceInTurn) {
+        int intForSwitch = [die.text intValue];
+
+        switch (intForSwitch) {
+            case 1:
+                one++;
+                NSLog(@"only once");
+                break;
+            case 2:
+                two++;
+                break;
+            case 3:
+                three++;
+                break;
+            case 4:
+                four++;
+                break;
+            case 5:
+                five++;
+                break;
+            case 6:
+                six++;
+                break;
+            default:NSLog(@"shid");
+                break;
+        }
+    }
+    if (one ==1 && two==1 && three==1 && four==1 && five==1 && six==1)
+    {
+        scoreKeeperInt += 1000;
+        self.userScore.text = [NSString stringWithFormat:@"%i",scoreKeeperInt];
+    }
+    else if (one == 3)
+    {
+        scoreKeeperInt +=1000;
+        self.userScore.text = [NSString stringWithFormat:@"%i",scoreKeeperInt];
+
+    }
+    else if (two ==3)
+    {
+        scoreKeeperInt +=200;
+        self.userScore.text = [NSString stringWithFormat:@"%i",scoreKeeperInt];
+
+    }
+    else if (three ==3)
+    {
+        scoreKeeperInt +=300;
+        self.userScore.text = [NSString stringWithFormat:@"%i",scoreKeeperInt];
+
+    }
+    else if (four ==3)
+    {
+        scoreKeeperInt +=400;
+        self.userScore.text = [NSString stringWithFormat:@"%i",scoreKeeperInt];
+
+    }
+    else if (five ==3)
+    {
+        scoreKeeperInt +=500;
+        self.userScore.text = [NSString stringWithFormat:@"%i",scoreKeeperInt];
+
+    }
+    else if (six ==3)
+    {
+        scoreKeeperInt +=600;
+        self.userScore.text = [NSString stringWithFormat:@"%i",scoreKeeperInt];
+
+    }
+
+    else if (five==1)
+    {
+        scoreKeeperInt +=50;
+        self.userScore.text = [NSString stringWithFormat:@"%i",scoreKeeperInt];
+
+    }
+    else if (one==1)
+    {
+        scoreKeeperInt +=100;
+        self.userScore.text = [NSString stringWithFormat:@"%i",scoreKeeperInt];
+
+    }
+}
+
+//- (void)selectDie:(id)die {
+//    [self.labels removeObject:die];
+//    [self.dice addObject:die];
+//}
 
 - (void)createView {
     self.dieContainer = [[UIView alloc] initWithFrame:CGRectMake(37, 200, 300, 300)];
@@ -61,8 +171,6 @@
 }
 
 - (void)callDynamics {
-
-
     [self.dieContainer addSubview:[self.labels objectAtIndex:0]];
     [self.dieContainer addSubview:[self.labels objectAtIndex:1]];
     [self.dieContainer addSubview:[self.labels objectAtIndex:2]];
@@ -70,16 +178,13 @@
     [self.dieContainer addSubview:[self.labels objectAtIndex:4]];
     [self.dieContainer addSubview:[self.labels objectAtIndex:5]];
 
-
     self.animator = [[UIDynamicAnimator alloc] initWithReferenceView:self.view];
-
 
     UIDynamicItemBehavior *itemBehavior = [[UIDynamicItemBehavior alloc] initWithItems:self.labels];
     itemBehavior.elasticity = 0.894;
     itemBehavior.angularResistance = 0.4;
     itemBehavior.friction = 0.2;
     itemBehavior.density = 0.5;
-
 
     UIGravityBehavior *gravityBehavior =[[UIGravityBehavior alloc] initWithItems:self.labels];
 
@@ -95,8 +200,9 @@
     [self.animator addBehavior:itemBehavior];
     [self.animator addBehavior:gravityBehavior];
     [self.animator addBehavior:collisionBehavior];
-
 }
+
+
 
 
 
